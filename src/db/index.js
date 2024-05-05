238d187499af8534b228f91b73e84c28cd228436
user.name "Krishna Chaurasia <user.email "krishnachaurasia045@gmail.com>
2024-05-06 04:40:43 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,26 @@
+// in this file we will connect database through mongoose 
+ import mongoose from "mongoose";
+ import { DB_NAME } from "../constants.js";
+
+
+ const connectDB = async () => {
+    try {
+        // mongoose gives return object; so we can store below code in variabel 
+        const connectionINstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)  // DB_NAME is name of DB
+    
+        console.log(`\n MongoDB connected !! DB HOST : ${connectionINstance.connection.host}`)
+    }
+
+    catch(error){
+        console.log("MONGODB connection failed", error);
+         
+        // nodejs provides access to process(can be used anywhere), 
+        // and our current application running on some process; 
+        // and below process.exit is reference of that process
+       
+        process.exit(1) // here 1 is exiting from node process failure
+    }
+ }
+
+
+ export default connectDB // now we can export this file 
---
+++ src/index.js
@@ -0,0 +1,83 @@
+// As early as possible in your application, import and configure dotenv:
+
+//require('dotenv').config ({path:'./env'})
+//or
+import dotenv from "dotenv"
+dotenv.config({
+    path: './env'
+})  // now got to package.json >> in script >> in dev's value add : '-r dotenv/config --experimental-json-modules' in b/w nodemon and src/index.js
+
+
+import mongoose, { connect } from "mongoose"; //mongoose helps to connect with database 
+import { DB_NAME } from "./constants.js"; // importing DB_NAME from constants.js file
+import connectDB from "./db/index.js";
+
+
+
+// connecting and exporting 2nd approach of DB
+connectDB() 
+
+
+
+
+
+
+
+
+/*       1st   Approach   of connecting MongoDB database 
+
+//**** sometime we initialize app which is formed from express 
+import express from "express";
+const app = express()
+
+
+//****  function for connection of Database
+ function connectDB(){ }  
+ //**execution of DB 
+ connectDB()
+
+ //**** better approach for DB connction & execution is IIFE(Immediately Invoked Function Expression)
+// (this is function i.e contains arrow function ) (immediate execution of function)
+// below arrow function is async
+
+
+// always give semi-colon(;) before the staring of IIFE function because if previous code doesn't have
+// ending ; then it may create problem
+ 
+
+
+;( async ()=>{
+
+    //provide try-catch because taling with DB
+    // if everything fine then execute try{ } else catch(){ }
+    
+    try {
+
+        //when we talk with .env file use process.env.Variable_nameto get access of variable
+        
+        await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)  // DB_NAME is name of DB
+        
+        //sometime we use app.on which has listener, it can listen like error   
+        // it is because our database is connected but our express's app is not able to communicate                                     
+        
+        app.on("error", (error)=>{    //(reveiving error) => { }
+            console.log("Error: our application(express) is able to talk to database : ",error);
+            throw error // throw error when not able to communicate
+        }) 
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    
+    }
+
+    catch(error){
+        console.error("ERROR: ", error) // or console.log("ERROR: ", error)
+        throw err // throw error 
+    }
+
+} ) ()
+
+*/
+
+
